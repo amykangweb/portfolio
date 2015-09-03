3e986bf21d64c7da7eabe33cc9d5e717b487deb9
amykangweb <selene6023@gmail.com>
2015-09-03 12:35:05 -0700

    Add tests for articles CRUD

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,9 @@
-ENV["RAILS_ENV"] = "test"
+Rails.env = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
 require "minitest/rails/capybara"
-
-# To add Capybara feature tests add `gem "minitest-rails-capybara"`
-# to the test group in the Gemfile and uncomment the following:
-# require "minitest/rails/capybara"
-
-# Uncomment for awesome colorful output
-# require "minitest/pride"
+require "minitest/pride"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
