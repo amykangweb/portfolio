586f5daf23513014f089c69ecfb2c444d6b24e42
amykangweb <selene6023@gmail.com>
2015-09-06 02:05:57 -0700

    Update index text to check for billboard text

--- test/features/index_test.rb
+++ test/features/index_test.rb
@@ -3,14 +3,10 @@ require "test_helper"
 class IndexSpecTest < Capybara::Rails::TestCase
   test "sanity" do
     visit root_path
-    within("h1") do
+    within(".header") do
       page.text.must_include "Amy Kang"
+      page.text.must_include "Designer / Developer"
     end
     refute_content page, "Goobye All!"
-  end
-
-  test "about" do
-    visit root_path
-    page.text.must_include "About"
   end
 end
