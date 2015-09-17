bb4b23ccead9e7421e7e93465326467a4e04d52d
amykangweb <selene6023@gmail.com>
2015-09-17 04:47:33 -0700

    Remove index and show methods from comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,7 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :set_comment, only: [:edit, :update, :destroy]
   before_action :set_article, only: [:edit, :create, :update]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-
-  # GET /comments
-  # GET /comments.json
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/1
-  # GET /comments/1.json
-  def show
-  end
 
   # GET /comments/1/edit
   def edit
