83876e8d220c6eb63fb6182ca0e6ef76d577d64b
amykangweb <selene6023@gmail.com>
2015-10-05 03:55:30 -0700

    Add social sharing gem

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'pundit'
 gem 'rails', '4.2.4'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'sidekiq'
+gem 'social-share-button'
 gem 'turbolinks'
 
 group :assets do
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require d3
+//= require social-share-button
 //= require foundation
 //= require turbolinks
 //= require_tree .
