becc1e9983f97b00edb11f0054bf5fa512255fde
amykangweb <selene6023@gmail.com>
2016-04-02 08:31:18 -0700

    Hide navbar brand on small browsers.

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -3,10 +3,10 @@ $(document).scroll(function(){
   console.log(a);
     if($(this).scrollTop() === 0)
   {
-    $('.navbar-inverse').css('background-color', 'transparent');
-    $('.navbar-inverse').css('border-color', 'transparent');
+    $('.main-navbar').css('background-color', 'transparent');
+    $('.main-navbar').css('border-color', 'transparent');
   } else {
-    $('.navbar-inverse').css('background-color', 'rgba(0, 0, 0, 0.85)');
-    $('.navbar-inverse').css('border-color', 'rgba(0, 0, 0, 0.85)');
+    $('.main-navbar').css('background-color', 'rgba(0, 0, 0, 0.85)');
+    $('.main-navbar').css('border-color', 'rgba(0, 0, 0, 0.85)');
   }
 });
