d5bed9989dead5d53b8f4e08506d13a412535aab
amykangweb <selene6023@gmail.com>
2016-04-02 13:55:41 -0700

    Add previous and next links for posts on posts show view.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ActiveRecord::Base
   default_scope -> { order('created_at DESC') }
   belongs_to :user
+
+  def previous
+    Post.all.detect { |post| post.id < self.id }
+  end
+
+  def next
+    Post.all.detect { |post| post.id > self.id }
+  end
+
 end
