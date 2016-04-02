05bec7b811033d5ab773f6de2f328b8d1848d3e9
amykangweb <selene6023@gmail.com>
2016-04-01 17:01:17 -0700

    Remove gravtastic reference from user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  include Gravtastic
-  gravtastic
   has_many :articles, foreign_key: "author_id"
   has_many :comments, dependent: :destroy
   # Include default devise modules. Others available are:
