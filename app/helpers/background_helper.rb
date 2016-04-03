39abfbe926645b6dd891dd48ebb0390431327552
amykangweb <selene6023@gmail.com>
2016-04-02 20:36:59 -0700

    Rename serious cat background image.

--- app/helpers/background_helper.rb
+++ app/helpers/background_helper.rb
@@ -2,7 +2,7 @@ module BackgroundHelper
 
   def random_bg
     ["catroll.png", "catwall.png", "combination.png", "code.png",
-      "coffeelaptop.png", "laptopopenbook.png", "seriouscat.png"].sample
+      "coffeelaptop.png", "laptopopenbook.png", "seriouscat1.png"].sample
   end
 
 end
