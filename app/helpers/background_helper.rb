4875a06a9506d4ec334d4981a092ae519e929835
amykangweb <selene6023@gmail.com>
2016-04-16 19:10:53 -0700

    Change laptopopenbook.png with jpg extension.

--- app/helpers/background_helper.rb
+++ app/helpers/background_helper.rb
@@ -2,7 +2,7 @@ module BackgroundHelper
 
   def random_bg
     ["catroll.jpg", "catwall.jpg", "combination.jpg", "code.jpg",
-      "coffeelaptop.jpg", "laptopopenbook.png", "nightsky.jpg", "seriouscat1.jpg"].sample
+      "coffeelaptop.jpg", "laptopopenbook.jpg", "nightsky.jpg", "seriouscat1.jpg"].sample
   end
 
 end
